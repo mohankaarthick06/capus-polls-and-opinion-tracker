d4020d7eb2e5f97ed07a3182a6a4fdf243fa7cf9
Mohan <mohankaarthick06@gmail.com>
2025-10-23 15:38:54 +0530

    creating PollOption.java

--- Campus-Polls-Opinion-Tracker/src/main/java/com/example/Campus/Polls/Opinion/Tracker/entity/PollOption.java
+++ Campus-Polls-Opinion-Tracker/src/main/java/com/example/Campus/Polls/Opinion/Tracker/entity/PollOption.java
@@ -1,5 +1,14 @@
 package com.example.Campus.Polls.Opinion.Tracker.entity;
-
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity
+@Data
 public class PollOption {
-    
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long optionId;
+private String optionText;
+@ManyToOne
+@JoinColumn(name="poll_id")
+private Poll poll; 
 }
