a77ff195b7e40ac133014ce09fae68c4dfc3975c
Mohan <mohankaarthick06@gmail.com>
2025-10-23 15:12:41 +0530

    structure for my project using spring iintialzr

---
+++ Campus-Polls-Opinion-Tracker/src/test/java/com/example/Campus/Polls/Opinion/Tracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Campus.Polls.Opinion.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
