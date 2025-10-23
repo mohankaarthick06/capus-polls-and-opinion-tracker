0ac70018307b59f52b42a036ed8e1b74b446ab8d
Mohan <mohankaarthick06@gmail.com>
2025-10-23 15:48:22 +0530

    creating vote.java

--- Campus-Polls-Opinion-Tracker/src/main/java/com/example/Campus/Polls/Opinion/Tracker/entity/Vote.java
+++ Campus-Polls-Opinion-Tracker/src/main/java/com/example/Campus/Polls/Opinion/Tracker/entity/Vote.java
@@ -1,5 +1,21 @@
 package com.example.Campus.Polls.Opinion.Tracker.entity;
-
+import jakarta.persistence.*;
+import  lombok.Data;
+import java.time.LocalDateTime;
+@Entity
+@Data
+@Table(uniqueConstraints={@UniqueConstraint(columnNames={"user_id","poll_id"})})
 public class Vote {
-    
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long voteId;
+@ManyToOne
+@JoinColumn(name="user_id")
+private User user;
+@ManyToOne@JoinColumn(name="poll_id")
+private Poll poll;
+@ManyToOne
+@JoinColumn(name="option_id")
+private PollOption option;
+private LocalDateTime votedAt=LocalDateTime.now();
 }
